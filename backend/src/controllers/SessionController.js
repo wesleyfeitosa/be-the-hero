b43e23827fc613a9a471735286fbd6307ac3cc4c
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-03-26 12:31:59 -0300

    finalizando o frontend

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -10,7 +10,7 @@ module.exports = {
     .first();
 
     if(!ong){
-      return res.status(400).json({error: 'No ONG found with thi ID'});
+      return res.status(400).json({error: 'No ONG found with this ID'});
     }
 
     return res.status(200).json(ong);
